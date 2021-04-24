c7db81bc7a59afae1524447dc5da9b14294050d6
unknown <Owner@NINOS.local>
2021-04-24 00:05:41 -0700

    md5 wasnt working out. refactored some code.

--- redis.go
+++ redis.go
@@ -17,6 +17,7 @@ type InsertData struct {
 }
 
 type HashArray []string
+type DbResponse []interface{}
 
 func NewRedisClient() (*Client, error) {
 	client := redis.NewClient(&redis.Options{
@@ -34,39 +35,41 @@ func NewRedisClient() (*Client, error) {
 	}, nil
 }
 
-func (c *Client) GetData(hArr *HashArray) (interface{}, error) {
-	rdb, ha := c.client, *hArr
+func (d *DbResponse) checkResponseValues() interface{} {
+	for _, val := range *d {
+		if val != nil && val != (*d)[0] {
+			return val
+		}
+	}
+	return nil
+}
 
-	dbResp, err := rdb.MGet(rdb.Context(), (ha)...).Result()
+func (c *Client) GetData(h *HashArray) (interface{}, error) {
+	var (
+		response DbResponse
+		err      error
+		rdb      *redis.Client = c.client
+	)
+	response, err = rdb.MGet(rdb.Context(), (*h)...).Result()
 	if err != nil {
 		return nil, err
 	}
-
-	firstItem := (ha)[0]
-	for _, val := range dbResp {
-		if val != nil && val != firstItem {
-			return val, nil
-		}
-	}
-	return nil, nil
+	go c.setData(InsertData{(*h)[0], (*h)[len(*h)-1], time.Duration(0)})
+	return response.checkResponseValues(), nil
 }
 
 func (c *Client) setData(d InsertData) error {
 	rdb := c.client
 
 	return rdb.Set(rdb.Context(), d.lastItem, d.firstItem, d.expiration).Err()
-
 }
 
-func (c *Client) CheckExist(hArr *HashArray) (int64, error) {
-	rdb, ha := c.client, *hArr
+// func (c *Client) CheckExist(h *HashArray) (int64, error) {
+// 	rdb, ha := c.client, *h
 
-	dbResp, err := rdb.Exists(rdb.Context(), ha...).Result()
+// 	dbResp, err := rdb.Exists(rdb.Context(), ha...).Result()
 
-	if err := c.setData(InsertData{ha[0], ha[len(ha)-1], time.Duration(0)}); err != nil {
-		return 0, err
-	}
+// 	go c.setData(InsertData{ha[0], ha[len(ha)-1], time.Duration(0)})
 
-	return dbResp, err
-
-}
+// 	return dbResp, err
+// }
--- server.go
+++ server.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"crypto/md5"
 	"crypto/sha256"
 	"encoding/hex"
 	"fmt"
@@ -18,21 +17,14 @@ type response struct {
 	Hash  string      `json:"hash"`
 }
 
-func (h *HashArray) hashThem() {
+func (h *HashArray) getHashes() {
 	gets256 := func(s string) string {
 		dig := sha256.Sum256([]byte(s))
-		return hex.EncodeToString(dig[:])
-	}
-	getmd5 := func(s string) string {
-		dig := md5.Sum([]byte(s))
 		return hex.EncodeToString(dig[:])
 	}
 	ha := *h
 	for i := 1; i < len(ha); i++ {
 		ha[i] = gets256(ha[i-1])
-	}
-	for i := 1; i < len(ha); i++ {
-		ha[i] = getmd5(ha[i])
 	}
 }
 
@@ -40,23 +32,16 @@ func (h *HashArray) getLastItem() string {
 	return (*h)[len(*h)-1]
 }
 
-func checkError(e error) {
-	if e != nil {
-		log.Fatal(e)
-		// log.Println("error checking if entry exists in redis or writing to db.", err)
-	}
-}
-
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU() / 4)
 
 	var (
 		redis *Client
-		rErr  error
+		err   error
 	)
 
-	if redis, rErr = NewRedisClient(); rErr != nil {
-		log.Fatal("Couldnt connect to redis instance", rErr)
+	if redis, err = NewRedisClient(); err != nil {
+		log.Fatal("Couldnt connect to redis instance", err)
 	}
 
 	app := fiber.New(fiber.Config{
@@ -64,33 +49,32 @@ func main() {
 	})
 	app.Use(cors.New())
 	app.Get("api/getdbsize", func(c *fiber.Ctx) error {
-		dbsize, e := redis.client.DBSize(redis.client.Context()).Result()
-		if e != nil {
-			return c.Next()
+		dbsize, err := redis.client.DBSize(redis.client.Context()).Result()
+		if err != nil {
+			return c.SendStatus(404)
 		}
 		return c.JSON(dbsize * 1000000)
 	})
+
 	app.Get("api/million/:id", func(c *fiber.Ctx) error {
 		length := 1000000 + 1
 		firstValue := c.Params("id")
 
-		hashArr := make(HashArray, length)
-		hashArr[0] = firstValue
-		hashArr.hashThem()
+		h := make(HashArray, length)
+		h[0] = firstValue
+		h.getHashes()
 
-		lastValue := hashArr.getLastItem()
-		exists, err := redis.CheckExist(&hashArr)
-		checkError(err)
-
-		if exists > 0 {
-			foundVal, err := redis.GetData(&hashArr)
-			checkError(err)
-
-			if foundVal != nil {
-				go Write2File(WriteFileStruct{fmt.Sprintf("seed: %v, hash: %v, lastItem: %v", foundVal, firstValue, lastValue), "/home/node/foundhashes.csv"})
-				return c.JSON(&response{true, foundVal, firstValue})
-			}
+		lastValue := h.getLastItem()
+		foundVal, err := redis.GetData(&h)
+		if err != nil {
+			return c.SendStatus(404)
 		}
+
+		if foundVal != nil {
+			go Write2File(WriteFileStruct{fmt.Sprintf("seed: %v, hash: %v, lastItem: %v", foundVal, firstValue, lastValue), "/home/node/foundhashes.csv"})
+			return c.JSON(&response{true, foundVal, firstValue})
+		}
+
 		go Write2File(WriteFileStruct{firstValue + "," + lastValue, "/home/node/newhashes.csv"})
 		return c.JSON(&response{false, "", firstValue})
 	})
